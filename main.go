27e36cc53b1fe6e152e3f5ea115dbec7961dbd53
liamchampton <liam.hampton@hotmail.co.uk>
2021-08-24 22:24:26 +0100

    create go routine and channel

--- main.go
+++ main.go
@@ -12,20 +12,27 @@ func main() {
 		"http://stackoverflow.com",
 		"https://golang.org",
 		"http://amazon.com",
+		"http://ibm.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
 
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "might be down I think!"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "Yep, it's up!"
 }
